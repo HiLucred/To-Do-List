54b51fea6c5e45cd47440fa96a71c6b74fd02386
Luc <lucred@protonmail.com>
2022-09-22 14:55:14 -0300

    Initial Commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,21 @@
+import styles from './Header.module.css'
+import rocketLogo from '../assets/rocket.svg'
+
+export function Header() {
+  return (
+    <header className={styles.header}>
+      <div className={styles.logo}>
+        <img
+          src={rocketLogo}
+          alt="Logo com a imagem de um foguete."
+        />
+
+        <h1>
+          to<span>do</span>
+        </h1>
+        
+      </div>
+
+    </header>
+  )
+}
---
+++ src/components/Task.tsx
@@ -0,0 +1,47 @@
+import styles from './Task.module.css'
+import { Trash } from 'phosphor-react'
+
+interface ITask {
+  id: string;
+  content: string;
+  isCompleted: boolean;
+  onDeleteTask: (content: string) => void;
+  onCompletedTask: (content: string) => void;
+}
+
+export function Task({ id, content, isCompleted, onDeleteTask, onCompletedTask }: ITask) {
+
+  function handleDeleteComment() {
+    onDeleteTask(id)
+  }
+
+  function handleCompleteTask() {
+    onCompletedTask(id)
+  }
+
+  return (
+    <div className={styles.card} >
+      <input
+        className={styles.checkbox}
+        id={id}
+        type="checkbox"
+        name="radio"
+        onChange={handleCompleteTask}
+      />
+
+      <label htmlFor={id}>
+        <p className={isCompleted ? styles.taskCompleted : styles.taskContent}>
+          {content}
+        </p>
+      </label>
+      
+      <button className={styles.deleteButton} onClick={handleDeleteComment}>
+        <Trash
+          size={20}
+          className={styles.trash}
+        />
+      </button>
+
+    </div>
+  )
+}
